2f4a7bbd827e9c6ae8c02c33524a35b9af3557a0
urahamat01 <urahamat01@gmail.com>
2019-06-25 13:48:40 +0600

    springHome

---
+++ SpringBoots/src/main/java/com/example/demo/Student.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "stu")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Student() {
+    }
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
